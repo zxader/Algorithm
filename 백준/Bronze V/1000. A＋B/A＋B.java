0695a6b5210fbbf5fc197a8da7bf8e2a405834b5
zxader <158558637+zxader@users.noreply.github.com>
2024-02-16 10:44:50 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze V/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		
+		String[] split = in.readLine().split(" ");
+		int A = Integer.parseInt(split[0]);
+		int B = Integer.parseInt(split[1]);
+		sb.append(A + B);
+		System.out.println(sb);
+	}
+}
