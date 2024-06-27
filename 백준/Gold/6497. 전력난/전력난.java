24af929c36b299294a7277593a0186d8b46058d4
zxader <158558637+zxader@users.noreply.github.com>
2024-06-27 14:24:58 +0900

    [Gold IV] Title: 전력난, Time: 964 ms, Memory: 263188 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static class Node implements Comparable<Node>{
+		int no, cost;
+		Node next;
+		public Node(int no, int cost, Node next) {
+			super();
+			this.no = no;
+			this.cost = cost;
+			this.next = next;
+		}
+		@Override
+		public String toString() {
+			return "Node [no=" + no + ", cost=" + cost + ", next=" + next + "]";
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.cost - o.cost;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int m = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			if (m == 0 && n == 0) {
+				break;
+			}
+			Node[] node = new Node[m];
+			long sum = 0;
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				int cost = Integer.parseInt(st.nextToken());
+				node[from] = new Node(to, cost, node[from]);
+				node[to] = new Node(from, cost, node[to]);
+				sum += cost;
+			}
+			
+			int[] min = new int[m];
+			Arrays.fill(min, Integer.MAX_VALUE);
+			boolean[] check = new boolean[m];
+			PriorityQueue<Node> q = new PriorityQueue<>();
+			min[0] = 0;
+			q.offer(new Node(0, 0, null));
+			long result = 0;
+			int cnt = 0;
+			while(!q.isEmpty()){
+				Node cur = q.poll();
+				if (check[cur.no]) continue;
+				check[cur.no] = true; 
+				result += min[cur.no];
+				for (Node temp = node[cur.no]; temp != null; temp = temp.next) {
+					if(!check[temp.no] && min[temp.no] > temp.cost) {
+						min[temp.no] = temp.cost;
+						q.offer(new Node(temp.no, min[temp.no], null));
+					}
+				}
+			}
+			sb.append(sum - result).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
