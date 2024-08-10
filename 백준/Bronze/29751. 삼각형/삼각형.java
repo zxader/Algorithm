30401a8463938ea6ede1cfd8bba15f4121058837
zxader <158558637+zxader@users.noreply.github.com>
2024-08-10 21:26:44 +0900

    [Bronze V] Title: 삼각형, Time: 64 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		sb.append((double)W*H/2);
+		System.out.println(sb);
+	}
+}
