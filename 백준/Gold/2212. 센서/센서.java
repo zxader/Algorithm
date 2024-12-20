b77da77644ac42e00613f2f5f5d0199254757f2b
zxader <158558637+zxader@users.noreply.github.com>
2024-12-20 23:17:01 +0900

    [Gold V] Title: 센서, Time: 156 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,44 +1,45 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // 입력 받기
-        int n = sc.nextInt(); // 센서의 개수
-        int k = sc.nextInt(); // 집중국의 개수
-
-        // 센서의 좌표 입력
-        int[] sensors = new int[n];
-        for (int i = 0; i < n; i++) {
-            sensors[i] = sc.nextInt();
+	static int N;
+	static int K;
+	static List<Integer> sensors;
+	static List<Integer> distances;
+	
+	public static void main(String[] args) throws Exception{
+		
+		// 입력 및 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine());
+		sensors = new ArrayList<>();
+		distances = new ArrayList<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			sensors.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		// 센서 좌표 정렬
+		Collections.sort(sensors);
+		
+		// 센서 간의 거리 계산
+		for (int i = 0; i < N - 1; i++) {
+            distances.add(sensors.get(i + 1) - sensors.get(i));
         }
-
-        // 센서 좌표 정렬
-        Arrays.sort(sensors);
-
-        // 센서 간의 거리 계산
-        int[] distances = new int[n - 1];
-        for (int i = 0; i < n - 1; i++) {
-            distances[i] = sensors[i + 1] - sensors[i];
-        }
-
-        // 거리 내림차순 정렬
-        Arrays.sort(distances);
-
-        // 집중국이 센서보다 많거나 같은 경우 영역 길이는 0
-        if (k >= n) {
-            System.out.println(0);
-            return;
-        }
-
-        // 가장 큰 (k-1)개의 거리를 제외한 합이 최소 영역 길이
-        int result = 0;
-        for (int i = 0; i < n - k; i++) {
-            result += distances[i];
-        }
-
-        // 결과 출력
-        System.out.println(result);
-    }
+		
+        // 거리 정렬
+		Collections.sort(distances);
+		
+		// 최소 영역 길이 구하기
+		int result = 0;
+		for (int i = 0; i < N - K; i++) {
+			result += distances.get(i);
+		}
+		
+		sb.append(result);
+		System.out.println(sb);
+	}
 }
