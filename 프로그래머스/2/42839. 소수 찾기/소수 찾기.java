16c63dfe82fe7d60e8f37f2b5ec400f34f7ce5ab
zxader <158558637+zxader@users.noreply.github.com>
2024-12-24 02:09:49 +0900

    [level 2] Title: 소수 찾기, Time: 23.65 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,44 @@
 import java.util.*;
 
 class Solution {
-    static Set<Integer> primeSet;
-    
+    private static String numbers; 
+    private static boolean[] visited; 
+    private static Set<Integer> primeSet;
+
     public int solution(String numbers) {
-        primeSet = new HashSet<>();
-        
-        perm("", numbers, new boolean[numbers.length()]);
-        
+        this.numbers = numbers; 
+        visited = new boolean[numbers.length()]; 
+        primeSet = new HashSet<>(); 
+
+        perm("");
+
         return primeSet.size(); // 소수의 개수 반환
     }
-    
-    // 순열
-    private void perm(String current, String numbers, boolean[] visited) {
+
+    // 순열 
+    private void perm(String current) {
         if (!current.isEmpty()) {
             int num = Integer.parseInt(current);
-            
             if (isPrime(num)) {
                 primeSet.add(num); // 소수이면 Set에 추가
             }
         }
-        
+
         for (int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                
-                perm(current + numbers.charAt(i), numbers, visited);
-                
-                visited[i] = false;
+            if (!visited[i]) { 
+                visited[i] = true; 
+                perm(current + numbers.charAt(i));
+                visited[i] = false; 
             }
         }
     }
-    
-    // 소수 판별
+
+    // 소수 판별 
     private boolean isPrime(int num) {
         if (num <= 1) return false;
-        
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) return false;
         }
-        
         return true;
     }
 }
