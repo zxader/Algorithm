e80b7d96b0460a31cb901fcc7a0b89944349d89e
zxader <158558637+zxader@users.noreply.github.com>
2025-01-08 06:50:17 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        
+        dp[1] = 1;
+        if (N >= 2) {
+        	dp[2] = 3;
+        }
+        
+        for (int i = 3; i <= N; i++) {
+        	dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+        
+        System.out.println(dp[N]);
+    }
+}
