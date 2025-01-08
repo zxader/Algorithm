d9d51261cf94cf38946522d19d56723d78a7c36f
zxader <158558637+zxader@users.noreply.github.com>
2025-01-08 18:46:04 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 146 ms, Memory: 24,032 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static class Point implements Comparable<Point>{
+		int r;
+		int c;
+		int v;
+		
+		public Point(int r, int c, int v) {
+			this.r = r;
+			this.c = c;
+			this.v = v;
+		}
+		
+		@Override
+		public int compareTo(Point o) {
+			return this.v - o.v;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int t = 1; t <= T; t++) {
+        	sb.append("#" + t + " ");
+        	int N = Integer.parseInt(br.readLine());
+        	int[][] map = new int[N][N];
+        	int[][] deltas = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        	int[][] dijk = new int[N][N];
+        	boolean[][] visited = new boolean[N][N];
+        	
+        	for (int i = 0; i < N; i++) {
+        		String input = br.readLine();
+        		for (int j = 0; j < N; j++) {
+        			map[i][j] = input.charAt(j) - '0';
+        			dijk[i][j] = Integer.MAX_VALUE;
+        		}
+        	}
+        	
+        	PriorityQueue<Point> q = new PriorityQueue<>();
+        	dijk[0][0] = 0;
+        	q.offer(new Point(0, 0, dijk[0][0]));
+        	
+        	while (!q.isEmpty()) {
+        		Point p = q.poll();
+        		
+        		for (int d = 0; d < 4; d++) {
+        			int dr = p.r + deltas[d][0];
+        			int dc = p.c + deltas[d][1];
+        			
+        			if (0 <= dr && dr < N && 0 <= dc && dc < N && 
+        					dijk[dr][dc] > dijk[p.r][p.c] + map[dr][dc]) {
+        				dijk[dr][dc] = dijk[p.r][p.c] + map[dr][dc]; 
+        				q.offer(new Point(dr, dc, dijk[dr][dc]));
+        			}
+        		}
+        	}
+        	
+        	sb.append(dijk[N - 1][N - 1]).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
