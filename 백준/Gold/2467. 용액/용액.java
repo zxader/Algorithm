475fc08b6712786e42eb05a217368cc8786db4c0
Taehun Kim <158558637+zxader@users.noreply.github.com>
2025-01-26 17:17:58 +0900

    [Gold V] Title: 용액, Time: 252 ms, Memory: 31240 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 입력
+        int N = Integer.parseInt(br.readLine()); // 전체 용액의 수
+        
+        int[] arr = new int[N];  // 용액의 특성값
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // 투포인터를 활용하여 두 용액 구하기
+        Arrays.sort(arr); // 정렬
+        
+        int left = 0;
+        int right = N - 1;
+        int resultLeft = arr[0]; // 0에 가까운 수를 만드는 왼쪽 용액 값
+        int resultRight = arr[N - 1]; // 0에 가까운 수를 만드는 오른쪽 용액 값
+        int result = Integer.MAX_VALUE; // 두 용액을 합해서  0에 가까운 값을 저장하기 위한 변수 
+        
+        while (left < right) {
+        	int sum = arr[left] + arr[right]; // 두 용액의 합
+        	
+        	if (Math.abs(sum) < result) {
+        		result = Math.abs(sum);
+        		resultLeft = arr[left];
+        		resultRight = arr[right];
+        	}
+        	
+        	if (sum > 0) {
+        		right--;
+        	}
+        	else {
+        		left++;
+        	}
+        }
+        
+        // 결과 출력
+        System.out.println(resultLeft + " " + resultRight);
+	}
+}
