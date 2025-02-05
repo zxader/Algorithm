bb69de2b4df6604f368de6f0d2d677781733557a
Taehun Kim <158558637+zxader@users.noreply.github.com>
2025-02-05 17:20:31 +0900

    [Silver I] Title: 오르막 수, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -13,10 +13,9 @@ public class Main {
         }
         
         for (int i = 2; i <= N; i++) {
-        	for (int j = 0; j < 10; j++) {
-        		for (int k = 0; k <= j; k++) {
-        			dp[i][j] += dp[i - 1][k] % 10007;
-        		}
+        	dp[i][0] = 1;
+        	for (int j = 1; j < 10; j++) {
+        		dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 10007;
         	}
         }
         
