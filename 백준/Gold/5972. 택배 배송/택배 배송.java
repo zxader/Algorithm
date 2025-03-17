e1b9d7ea175817a408797df28e5011493ff325a3
Taehun Kim <158558637+zxader@users.noreply.github.com>
2025-03-17 20:11:46 +0900

    [Gold V] Title: 택배 배송, Time: 364 ms, Memory: 39052 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static class Node implements Comparable<Node>{
+		int no, value;
+		Node next;
+		
+		public Node(int no, Node next, int value) {
+			this.no = no;
+			this.next = next;
+			this.value = value;
+		}
+		
+		@Override
+		public int compareTo(Node o) {
+			return this.value - o.value;
+		}
+	}
+	static int N;
+	static int M;
+	static Node[] node;
+	
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	node = new Node[N + 1];
+    	
+    	for (int i = 0; i < M; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int from = Integer.parseInt(st.nextToken());
+    		int to = Integer.parseInt(st.nextToken());
+    		int value = Integer.parseInt(st.nextToken());
+    		node[from] = new Node(to, node[from], value);
+    		node[to] = new Node(from, node[to], value);
+    	}
+    	
+    	System.out.println(start());
+    }
+    
+    static int start() {
+    	int[] minValue = new int[N + 1];
+    	boolean[] visited = new boolean[N + 1];
+    	Arrays.fill(minValue, Integer.MAX_VALUE);
+    	PriorityQueue<Node> q = new PriorityQueue<>();
+    	q.offer(new Node(1, null, 0));
+    	minValue[1] = 0;
+    	
+    	while (!q.isEmpty()) {
+    		Node p = q.poll();
+    		
+    		visited[p.no] = true;
+    		
+    		for (Node temp = node[p.no]; temp != null; temp = temp.next) {
+    			if (minValue[temp.no] > minValue[p.no] + temp.value) {
+    				minValue[temp.no] = minValue[p.no] + temp.value;
+    				q.offer(new Node(temp.no, null, temp.value));
+    			}
+    		}
+    	}
+    	
+    	return minValue[N];
+    }
+}
