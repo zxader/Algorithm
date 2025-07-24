608cc61ddf463445f019ceed016b7b88ae4a4d0f
Taehun Kim <158558637+zxader@users.noreply.github.com>
2025-07-24 21:55:51 +0900

    [Silver III] Title: Four Squares, Time: 92 ms, Memory: 12080 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -10,12 +10,9 @@ public class Main {
     	int[] dp = new int[n + 1];
     	Arrays.fill(dp, 4);
     	dp[0] = 0;
-    	dp[1] = 1;
     	
-    	for (int i = 2; i <= n; i++) {
-    		int num = 1;
+    	for (int i = 1; i <= n; i++) {
     		for (int j = (int)Math.sqrt(i); j >= 1; j--) {
-    			if (i - j * j < 0) continue;
     			dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
     		}
     	}
