97462a62909b1eaf3f0263ed6a06db7741c4a761
Taehun Kim <158558637+zxader@users.noreply.github.com>
2025-08-11 10:33:40 +0900

    [Gold V] Title: 동전, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] arr;
+	static int N;
+	static int[][] dp;
+	public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	StringBuilder sb = new StringBuilder();
+    	int T = Integer.parseInt(br.readLine());
+    	for (int t = 1; t <= T; t++) {
+    		N = Integer.parseInt(br.readLine());
+    		arr = new int[N + 1];
+    		
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		for (int i = 1; i <= N; i++) {
+    			arr[i] = Integer.parseInt(st.nextToken());
+    		}
+    		
+    		int M = Integer.parseInt(br.readLine());
+    		dp = new int[N + 1][M + 1];
+    		
+    		for (int i = 0; i <= N; i++) {
+    			dp[i][0] = 1;
+    		}
+    		
+    		for (int i = 1; i <= N; i++) {
+    			for (int j = 1; j <= M; j++) {
+    				
+    				int sum = 0;
+    				int temp = j;
+    				
+    				while (temp >= 0) {
+    					sum += dp[i - 1][temp];
+    					temp -= arr[i];
+    				}
+    				
+    				dp[i][j] = sum;
+    			}
+    		}
+    		
+    		sb.append(dp[N][M]).append("\n");
+    	}
+    	
+    	System.out.println(sb);
+	}
+}
