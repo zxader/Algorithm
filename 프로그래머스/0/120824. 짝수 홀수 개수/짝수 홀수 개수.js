f9ce23e85077cdbd09a5cda42cdcb1a17260f680
Taehun Kim <158558637+zxader@users.noreply.github.com>
2026-06-06 00:57:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.12 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let a = 0;
+    let b = 0;
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2 ==0) {
+            a++;
+        }
+        else {
+            b++;
+        }
+    }
+    var answer = [a, b];
+    return answer;
+}
