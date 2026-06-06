c57d4556e4267becc506b352488ee502ddc18afd
Taehun Kim <158558637+zxader@users.noreply.github.com>
2026-06-07 05:20:33 +0900

    [level 2] Title: 기능개발, Time: 0.25 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript) 
 
 ### 성능 요약
 
-메모리: 83.1 MB, 시간: 0.19 ms
+메모리: 32.4 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 20일 15:54:57
+2026년 06월 07일 05:20:34
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,25 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    
+
+    
+    while(progresses.length > 0) {
+        
+        for (let i = 0; i < progresses.length; i++) {
+            progresses[i] += speeds[i];
+        }
+        
+        let count = 0;
+        while(progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            count++;
+        }
+        
+        if (count >= 1) {
+            answer.push(count);
+        }
+    }
+    
+    return answer;
+}
